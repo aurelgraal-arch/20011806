9b805f1e0c6fd685208860e75eacd9da6e0e515c
aurelgraal-arch <aurelgraal@gmail.com>
2026-02-05 01:14:30 +0100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-ts'
+import { createClient } from "@supabase/supabase-js"
 
-export const supabase = createClient(
-  'https://vayuwijyxnezpusaqjmz.supabase.co', // URL del tuo progetto Supabase
-    'sb_publishable_lYqCuU3Mg1uHE42wo2RMfg_icigb8z5' // Publishable Key
-    )
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
